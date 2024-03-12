df2d97c82d8a1c78e0ef0e218c3a6a058584b86a
gianlucafalaschi <gian100gian@outlook.it>
2024-03-12 16:03:30 +0100

    2 snack

---
+++ snack-2/js/script.js
@@ -0,0 +1,17 @@
+// - L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// chiedo 2 volte una parola all'utente
+for(let i = 0; i < 2; i++){
+    let userWord = prompt('Dammi una parola');
+    // trovo la lunghezza della parola
+    let userWordLength = userWord.length;
+    console.log(userWordLength);
+    //controllo quale parola e' piu' corta 
+    
+    // se la parola piu' corta e' la prima la stampo
+        // poi stampo la seconda
+    //altrimenti stampo prima seconda parola 
+        // e poi stampo la prima
+}
+
