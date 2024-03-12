ddef0362fdd8f2bf643c1a180462ab81bac92d01
gianlucafalaschi <gian100gian@outlook.it>
2024-03-12 15:29:50 +0100

    feat: snack 1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,17 @@
 // - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 //se è dispari stampa il numero successivo
 
+// chiedo un numero all'utente
+let userNumber = parseInt(prompt('Dammi un numero'));
+//controllo se il numero e' pari
+    if(userNumber % 2 === 0) {
+        // se il numero e' pari stampo il numero
+        console.log(userNumber);
+    }
+    //altrimenti stampo il numero successivo
+    else{
+        userNumber = userNumber + 1;
+        console.log(userNumber);
+    }
+    
+    
