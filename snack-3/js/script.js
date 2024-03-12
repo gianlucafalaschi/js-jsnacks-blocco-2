e6bc8cd2ff926f8b4f49a7a89b6b718de53e3cb8
gianlucafalaschi <gian100gian@outlook.it>
2024-03-12 17:31:02 +0100

    snack 3: array created

---
+++ snack-3/js/script.js
@@ -0,0 +1,17 @@
+//- Crea due tag div con due id diversi:
+//un div avrà il testo colorato di rosso mentre l’altro di verde.
+//Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+// CREO UN ARRAY DI NUMERI
+let numbersArray = [20, 33, 44, 55, 100, 105, 99, 120, 1000, 3];
+// creo una costante per il numero di elementti dell'array
+const arraySize = numbersArray.length;
+console.log(arraySize);
+
+// scorro tutto gli elementi dell'array e controllo se i numeri sono dispari o pari
+//for(let i = 0; i )
+
+// STAMPO NELL'ID ROSSO I NUMERI PARI 
+
+// STAMPO NELL'ID VERDE I NUMERI DISPARI
+
