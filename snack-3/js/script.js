fbea42ec42e6af126f96825d23b911e8a2c6a527
gianlucafalaschi <gian100gian@outlook.it>
2024-03-12 17:43:21 +0100

    feat: snack 3 conditional

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -3,13 +3,24 @@
 //Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
 // CREO UN ARRAY DI NUMERI
-let numbersArray = [20, 33, 44, 55, 100, 105, 99, 120, 1000, 3];
-// creo una costante per il numero di elementti dell'array
+let numbersArray = [20, 33, 44, 55, 100, 105, 99, 120, 1000, 3, 7];
+// creo una costante per il numero di elementi dell'array
 const arraySize = numbersArray.length;
-console.log(arraySize);
 
-// scorro tutto gli elementi dell'array e controllo se i numeri sono dispari o pari
-//for(let i = 0; i )
+// scorro tutto gli elementi dell'array 
+for(let i = 0; i < arraySize; i++) {
+    let thisNumber = numbersArray[i];
+    //controllo se i numeri sono dispari
+    //se sono dispari li metto nel div rosso
+    if(thisNumber % 2 !== 0) {
+        console.log('il numero è dispari e va nel div rosso', thisNumber);
+    } 
+    //altrimenti li metto nel div verde
+      else {
+        console.log('il numero è pari e va nel div verde', thisNumber);
+      }  
+        
+}
 
 // STAMPO NELL'ID ROSSO I NUMERI PARI 
 
