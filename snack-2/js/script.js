9f1db63663c3b7b63bc035b05da246fdd08bfc62
gianlucafalaschi <gian100gian@outlook.it>
2024-03-12 16:48:58 +0100

    fix: snack 2 logic

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,24 +1,6 @@
 // - L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
-/* 
-// chiedo 2 volte una parola all'utente
-for(let i = 0; i < 2; i++){
-    let userWord = prompt('Dammi una parola');
-    // trovo la lunghezza della parola
-    let userWordLength = userWord.length;
-    console.log(userWordLength);
-    //controllo quale parola e' piu' corta 
-    if(userWordLength > userWordLength) {
-        console.log('prima parola', )
-    }
-    // se la parola piu' corta e' la prima la stampo
-        // poi stampo la seconda
-    //altrimenti stampo prima seconda parola 
-        // e poi stampo la prima
-}
 
-
- */
 
 // chiedo 2 volte una parola all'utente
 let firstUserWord = prompt('Dammi una prima parola');
@@ -26,14 +8,14 @@ let secondtUserWord = prompt('Dammi una seconda parola');
 
 // trovo la lunghezza della parola
 // se la parola piu' corta e' la prima la stampo
-if(firstUserWord.length > secondtUserWord.length) {
-    console.log(firstUserWord);
+if(firstUserWord.length < secondtUserWord.length) {
+    console.log("La parola piu' corta è ", firstUserWord);
     // poi stampo la seconda
-    console.log(secondtUserWord);    
+    console.log("La parola piu' lunga è ", secondtUserWord);    
 }
 //altrimenti stampo prima seconda parola 
         // e poi stampo la prima
  else{
-    console.log(secondtUserWord);    
-    console.log(firstUserWord);
+    console.log("La parola piu' corta è ", secondtUserWord);    
+    console.log("La parola piu' lunga è ", firstUserWord);
 }
